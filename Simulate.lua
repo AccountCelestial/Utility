3457bf9c8e7f8f36650ab8327e2af5a847d4cc7a
AccountCelestial <v2mjpqjfry@privaterelay.appleid.com>
2025-12-10 15:06:57 +0900

    Update Simulate.lua

--- Simulate.lua
+++ Simulate.lua
@@ -1,87 +1,90 @@
-local SimulateLib = {};
+local SimulateLib = {}
 
-local ServiceLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/AccountCelestial/Utility/refs/heads/main/Services.lua'))();
+local ServiceLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/AccountCelestial/Utility/refs/heads/main/Services.lua'))()
 
-local VIM, GuiService, Players, VirtualUser, CurrentCamera = ServiceLib:Get('VirtualInputManager', 'GuiService', 'Players', 'VirtualUser');
-local LocalPlayer = Players.LocalPlayer;
-
-getgenv().AFK = false;
+local VIM, GuiService, Players, VirtualUser, CurrentCamera = ServiceLib:Get('VirtualInputManager', 'GuiService', 'Players', 'VirtualUser')
+local LocalPlayer = Players.LocalPlayer
 
 function SimulateLib:Click(target, eventName, mode)
     if mode == 'firesignal' and firesignal and target and target[eventName] then
-        local ok, err = pcall(function() firesignal(target[eventName]) end);
-        if not ok then warn('firesignal error: ', err); end;
+        local ok, err = pcall(function() firesignal(target[eventName]) end)
+        if not ok then warn('firesignal error: ', err) end
     elseif mode == 'replicatesignal' and replicatesignal and target and target[eventName] then
-        local ok, err = pcall(function() replicatesignal(target[eventName]) end);
-        if not ok then warn('replicatesignal error: ', err); end;
+        local ok, err = pcall(function() replicatesignal(target[eventName]) end)
+        if not ok then warn('replicatesignal error: ', err) end
     elseif mode == 'vim' and target and target[eventName] then
         local ok, err = pcall(function()
-            GuiService.SelectedObject = target;
-            task.wait();
-            VIM:SendKeyEvent(true, Enum.KeyCode.Return, false, game);
-            task.wait();
-            VIM:SendKeyEvent(false, Enum.KeyCode.Return, false, game);
-            task.wait();
-            GuiService.SelectedObject = nil;
-        end);
-        if not ok then warn('VIM MosueClick error: ', err); end;
+            GuiService.SelectedObject = target
+            task.wait()
+            VIM:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+            task.wait()
+            VIM:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+            task.wait()
+            GuiService.SelectedObject = nil
+        end)
+        if not ok then warn('VIM MosueClick error: ', err) end
     elseif mode == 'getconnections' and getconnections and target and target[eventName] then
         for _, v in pairs(getconnections(target[eventName])) do
             if v.Function then 
-                local ok, err = pcall(v.Function); 
-                if not ok then warn('Function error: ', err); end;
-            end;
+                local ok, err = pcall(v.Function) 
+                if not ok then warn('Function error: ', err) end
+            end
             if v.Fire then 
-                local ok, err = pcall(function() v:Fire(); end); 
-                if not ok then warn('Fire error: ', err); end;
-            end;
-        end;
-    end;
-end;
+                local ok, err = pcall(function() v:Fire() end) 
+                if not ok then warn('Fire error: ', err) end
+            end
+        end
+    end
+end
 
 function SimulateLib:SendKeyEvent(key, time)
     local ok, err = pcall(function()
-        VIM:SendKeyEvent(true, Enum.KeyCode[key], false, game);
-        task.wait(time or nil);
-        VIM:SendKeyEvent(false, Enum.KeyCode[key], false, game);
-    end);
-    if not ok then warn('VIM KeyPress error: ', err); end;
-end;
+        VIM:SendKeyEvent(true, Enum.KeyCode[key], false, game)
+        task.wait(time or nil)
+        VIM:SendKeyEvent(false, Enum.KeyCode[key], false, game)
+    end)
+    if not ok then warn('VIM KeyPress error: ', err) end
+end
 
 function SimulateLib:Fire(target, eventName, mode)
     if mode == 'fireproximityprompt' and fireproximityprompt and target then
         local ok, err = pcall(function()
-            fireproximityprompt(target);
-        end);
-        if not ok then warn('fireproximityprompt error: ', err); end;
+            fireproximityprompt(target)
+        end)
+        if not ok then warn('fireproximityprompt error: ', err) end
     elseif mode == 'fireclickdetector' and fireclickdetector and target then
         local ok, err = pcall(function()
-            fireclickdetector(target);
-        end);
-        if not ok then warn('fireclickdetector error: ', err); end;
+            fireclickdetector(target)
+        end)
+        if not ok then warn('fireclickdetector error: ', err) end
     elseif mode == 'firetouchinterest' and firetouchinterest and target then
-        local hrp = LocalPlayer.Character:FindFirstChild('HumanoidRootPart');
+        local hrp = LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
         local ok, err = pcall(function()
-            firetouchinterest(hrp, target, 0);
-            task.wait();
-            firetouchinterest(hrp, target, 1);
-        end);
-        if not ok then warn('firetouchinterest error: ', err); end;
-    end;
-end;
+            firetouchinterest(hrp, target, 0)
+            task.wait()
+            firetouchinterest(hrp, target, 1)
+        end)
+        if not ok then warn('firetouchinterest error: ', err) end
+    end
+end
+
+local afkConnect = nil
 
 function SimulateLib:Player(state, mode)
-    if mode == 'AFK' then
-        getgenv().AFK = state;
-    end;
-end;
+    if mode == 'afk' then
+        if state then
+            afkConnect = LocalPlayer.Idled:Connect(function()
+                VirtualUser:Button2Down(Vector2.new(0,0), CurrentCamera.CFrame)
+                task.wait(1)
+                VirtualUser:Button2Up(Vector2.new(0,0), CurrentCamera.CFrame)
+            end)
+        else
+            if afkConnect then
+                afkConnect:Disconnect()
+                afkConnect = nil
+            end
+        end
+    end
+end
 
-LocalPlayer.Idled:Connect(function()
-    if getgenv().AFK then
-        VirtualUser:Button2Down(Vector2.new(0,0), CurrentCamera.CFrame);
-        task.wait(1);
-        VirtualUser:Button2Up(Vector2.new(0,0), CurrentCamera.CFrame);
-    end;
-end);
-
-return SimulateLib;
+return SimulateLib
